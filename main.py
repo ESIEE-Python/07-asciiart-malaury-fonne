dcdb90d8002c98c244340c8b6960a1800241a25f
malaury-fonne <malaury.fonne@edu.esiee.fr>
2024-09-30 11:46:07 +0000

    travail demandé sur l'encodage

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+"""Fonction qui permet d'encoder une chaine de charactere"""
 #### Imports et définition des variables globales
-
+import sys
+sys.setrecursionlimit(10000)
 
 #### Fonctions secondaires
 
 
-def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+def artcode_i(s:str):
+    """retourne la liste de tuples encodant
+     une chaîne de caractères passée en argument 
+     selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -13,14 +17,30 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
     # votre code ici
-
-    return [ ]
+    l=[]
+    n=1
+    if len(s)==1:
+        l.append((s[0],1))
+        return l
+    for i in range(1,len(s)):
+        if s[i]==s[i-1]:
+            n=n+1
+        else:
+            if i == len(s)-1:
+                l.append((s[i-1],n))
+                l.append((s[i],1))
+            else:
+                l.append((s[i-1],n))
+            n=1
+    if n>1:
+        l.append((s[len(s)-1],n))
+    return l
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne 
+    de caractères passée en argument selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -28,22 +48,32 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
     # votre code ici
 
     # cas de base
     # recherche nombre de caractères identiques au premier
     # appel récursif
-
-    return []
-    
+    l=[]
+    n=1
+    if len(s)==1:
+        l.append((s[0],1))
+        return l
+    while n < len(s) and s[0]==s[n]:
+        n=n+1
+    l.append((s[0],n))
+    if n==len(s) and s[0]==s[n-1]:
+        return l
+    return l+artcode_r(s[n:])
 
 #### Fonction principale
 
 
 def main():
+    """Execute et affiche pour des valeurs tests"""
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
+    print(artcode_i('vbhibeiRRRRttt   UUUUU'))
+    print(artcode_r('vbhibeiRRRRttt   UUUUU'))
 
 if __name__ == "__main__":
     main()
